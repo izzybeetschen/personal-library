a03cf141d76344596439c0221d1c2071a75b67ba
izzybeetschen <114227602+izzybeetschen@users.noreply.github.com>
2025-02-07 16:09:23 +0000

    Commented out sql to fix database

--- library.py
+++ library.py
@@ -1,23 +1,20 @@
-import mysql.connector
+# import mysql.connector
 
-# Connect to the MySQL database
-conn = mysql.connector.connect(
-    host="localhost",       # Database host
-    user="izzy",            # Your MySQL username
-    password="",  # Your MySQL password
-    database="book_collection_db"  # The database you're using
-)
+# # Connect to the MySQL database
+# conn = mysql.connector.connect(
+#     host="localhost",       # Database host
+#     user="izzy",            # Your MySQL username
+#     password="",  # Your MySQL password
+#     database="book_collection_db"  # The database you're using
+# )
 
-cursor = conn.cursor()
+# cursor = conn.cursor()
 
-# Example: Fetch all books from the books table
-cursor.execute("SELECT * FROM books")
-books = cursor.fetchall()
+# cursor.execute("SELECT * FROM books")
+# books = cursor.fetchall()
 
-# Print out the results (books)
-for book in books:
-    print(book)
+# for book in books:
+#     print(book)
 
-# Close the connection
-cursor.close()
-conn.close()
+# cursor.close()
+# conn.close()
